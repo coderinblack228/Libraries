152bc5267d576e143e4d8bec9381e630bd161696
LURKLURKLURKNOTLURK <120607422+LURKLURKLURKNOTLURK@users.noreply.github.com>
2023-01-15 21:58:15 -0500

    Update database.lua

--- Vanity/database.lua
+++ Vanity/database.lua
@@ -12,9 +12,13 @@ local function add_staff(data)
 end
 
 add_staff({user = 'KitcheRules', reason = 'Moderation association (Friends with staff)'})
+add_staff({user = 'CheaterClarifyEL', reason = 'Moderation association (Friends with staff)'})
+add_staff({user = 'Trimti', reason = 'Staff member, not much known, unmarked account'})
+add_staff({user = 'WarthogNation6', reason = 'Staff member, not much known, unmarked account'})
+--[[
+** Currently removed until they move their accounts out of the groups 
 add_staff({user = 'alphaplayer91', reason = 'High ranking staff, quick to abuse his power. The SteffJonez of Vanity.'})
 add_staff({user = 'SteveGuy477', reason = 'Highly active staff member, moderates a lot. Quick to ban, will ban for any reason.'})
-add_staff({user = 'Trimti', reason = 'Staff member, not much known.'})
 add_staff({user = 'NoProfits', reason = 'Group holder for vanity'})
 add_staff({user = 'VanityFPS', reason = 'Developer'})
 add_staff({user = 'HiLzD', reason = 'Developer'})
@@ -32,5 +36,5 @@ add_staff({user = 'BTagent', reason = 'Content creator.'})
 add_staff({user = 'NoloRave', reason = 'Developer'})
 add_staff({user = 'MASTERKSKHA', reason = 'Developer'})
 add_staff({user = 'Slightly_Vexed', reason = 'Developer'})
-add_staff({user = 'Games_Incorporated', reason = 'Developer'})
+add_staff({user = 'Games_Incorporated', reason = 'Developer'})]]
 return database
